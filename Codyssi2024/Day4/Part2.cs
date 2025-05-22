a5851b83fcb72bdeef6288d3b44493844a07e1bd
sb17057 <sbarnes@vermeer.com>
2025-05-22 13:16:30 -0500

    day 4 init

---
+++ Codyssi2024/Day4/Part2.cs
@@ -0,0 +1,12 @@
+namespace Codyssi2024.Day4;
+
+public class Part2() : BasePart(4,2,true)
+{
+    public override string Run()
+    {
+        //var input = Input();
+        //var input = InputChars();
+
+        return 0.ToString();
+    }
+}
