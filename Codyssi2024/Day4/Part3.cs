ddfffbb90397376929b00c0c8f084f7bb5d31b56
sb17057 <sbarnes@vermeer.com>
2025-05-23 08:24:12 -0500

    day 4

--- Codyssi2024/Day4/Part3.cs
+++ Codyssi2024/Day4/Part3.cs
@@ -1,11 +1,12 @@
 // ReSharper disable SuspiciousTypeConversion.Global
+
 namespace Codyssi2024.Day4;
 
-public class Part3() : BasePart(4,3,true)
+public class Part3() : BasePart(4, 3)
 {
     public override string Run()
     {
-         var input = Input().Select(x => x.Split(" <-> "));
+        var input = Input().Select(x => x.Split(" <-> "));
         var locations = new List<Node>();
         foreach (var item in input)
         {
@@ -43,23 +44,21 @@ public class Part3() : BasePart(4,3,true)
 
         var start = locations.Find(x => x.Equals("STT"))!;
 
-        var q = new PriorityQueue<(Node node,int dist), int>(Comparer<int>.Create((a,b)=>a-b));
+        var q = new PriorityQueue<(Node node, int dist), int>(Comparer<int>.Create((a, b) => a - b));
         var distances = new Dictionary<string, int>();
         foreach (var location in locations)
-            distances.Add(location.Name, Int32.MaxValue);
-        q.Enqueue((start,0), 0);
+            distances.Add(location.Name, int.MaxValue);
+        q.Enqueue((start, 0), 0);
         while (q.Count > 0)
         {
             var (current, dist) = q.Dequeue();
-            distances[current.Name] = dist;
+            if (distances[current.Name] > dist)
+                distances[current.Name] = dist;
             foreach (var path in current.Links)
-            {
-                if(distances[path.Name] < dist+1)
-                    q.Enqueue((path,dist+1), dist+1);
-            }
+                if (distances[path.Name] > dist + 1)
+                    q.Enqueue((path, dist + 1), dist + 1);
         }
 
-
-        return "";
+        return distances.Values.Sum().ToString();
     }
 }
